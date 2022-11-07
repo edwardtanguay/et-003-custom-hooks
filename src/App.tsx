4306f8cef725b9ad52ebe72100236825e1d42a9d
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-07 21:43:19 +0100

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.scss';
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Custom Hooks Site</h1>
+			<p>Welcome to this site.</p>
+		</div>
+	);
+}
+
+export default App;
