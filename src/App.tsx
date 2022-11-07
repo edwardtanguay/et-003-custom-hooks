88f1b08cf0f41efca2b7af405e42d737f695fcfe
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-07 22:15:12 +0100

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import './App.scss';
+import { NavLink, Routes, Route, Navigate } from 'react-router-dom';
+import { PageWelcome } from './components/PageWelcome';
+import { PageUseToggle } from './components/PageUseToggle';
+import { PageUseFetch } from './components/PageUseFetch';
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Custom Hooks Site</h1>
-			<p>Welcome to this site.</p>
+			<nav>
+				<NavLink to="/welcome">Welcome</NavLink> 
+				<NavLink to="/useToggle">useToggle</NavLink>
+				<NavLink to="/useFetch">useFetch</NavLink>
+			</nav>
+			<Routes>
+				<Route path="/welcome" element={<PageWelcome />} />
+				<Route path="/useToggle" element={<PageUseToggle />} />
+				<Route path="/useFetch" element={<PageUseFetch />} />
+				<Route path="/" element={<Navigate to="/welcome" replace />} />
+			</Routes>
 		</div>
 	);
 }
