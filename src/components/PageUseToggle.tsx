04077623f321a3e9a307d529f0fefa97e43a2531
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-07 23:04:24 +0100

    advertisements

--- src/components/PageUseToggle.tsx
+++ src/components/PageUseToggle.tsx
@@ -1,7 +1,14 @@
+import { useState } from 'react';
+
 export const PageUseToggle = () => {
+	const [showAdvertisements, setShowAdvertisements] = useState(false);
+
 	return (
 		<>
 			<p>The useToggle page.</p>
+
+			<h2>User Settings</h2>
+			<p><button onClick={() => setShowAdvertisements(!showAdvertisements)}>Advertisements</button> <span style={{ color: showAdvertisements ? 'green' : 'red' }}> {showAdvertisements ? 'yes, show advertisements' : 'no, do not show advertisements'}</span></p>
 		</>
 	);
 };
